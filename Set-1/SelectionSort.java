6c201c77d79f56f35f440d34a512310a2e110bc9
Pranita-svg <pranita2257@gmail.com>
2025-12-23 21:27:11 +0530

    Create SelectionSort.java
    This is my code for sorting an array of n elements (user input) using Selection Sort.

---
+++ Set-1/SelectionSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class selectionsort{
+static void ss(int a[])
+  { for(int i=0;i<a.length;i++){
+  int min=i;
+  for(int j=i+1;j<a.length;j++)
+    if(a[j]<a[min])
+      min=j;
+
+  int t=a[i];
+  a[i]=a[min];
+  a[min]=t;
+}
+}
+
+static void ap(int a[]){
+  for(int i=0;i<a.length;i++)
+    System.out.print(a[i]);
+  System.out.println();
+}
+
+
+public static void main(String args[]){
+ Scanner in=new Scanner(System.in);
+  System.out.print("Enter the number of elements you want to add in the array");
+  int n=in.nextInt();
+  int a[]=new int[n];
+  for(int i=0;i<n;i++){
+  System.out.println("Enter an element");
+  int x=in.nextInt();
+  a[i]=x;
+  }
+  ss(a);
+  ap(a);
+}
+}
+  
